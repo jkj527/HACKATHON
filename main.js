b87d884a37a5c6940a0fc371216f59b56351a6d9
Jake Johnson <jkjohnson1996@gmail.com>
2023-10-12 11:03:39 -0700

    added gif flip

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ document.addEventListener('mousemove', (e) => {
         const x = e.clientX;
         const y = e.clientY;
 
+        const centerX = window.innerWidth / 2;
+
+        // Check if the cursor is on the left or right half of the screen
+        if (x < centerX) {
+            // Cursor is on the left half, so flip the follower horizontally
+            follower.style.transform = 'scaleX(-1)';
+        } else {
+            // Cursor is on the right half, reset the transformation
+            follower.style.transform = 'none';
+        }
+
         follower.style.left = x + 'px';
         follower.style.top = y + 'px';
     // })
