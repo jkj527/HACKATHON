a593ec0234b03970296ad4643b2700c7f67d5201
Jake Johnson <jkjohnson1996@gmail.com>
2023-10-12 17:00:21 -0700

    button update

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById('checkButton');
+    
     btn.addEventListener('click', function() {
         alert("CAT");
     });
